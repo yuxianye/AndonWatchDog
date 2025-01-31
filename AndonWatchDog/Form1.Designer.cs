0e6577bd44a8e9199d0b19a143823eefb7e580ba
yuxianye <yuxianye@163.com>
2025-01-31 17:12:09 +0800

    添加项目文件。

---
+++ AndonWatchDog/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace AndonWatchDog
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showMainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nud_interval = new System.Windows.Forms.NumericUpDown();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_interval)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Anodn WatchDog";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showMainToolStripMenuItem,
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(141, 92);
+            // 
+            // showMainToolStripMenuItem
+            // 
+            this.showMainToolStripMenuItem.Name = "showMainToolStripMenuItem";
+            this.showMainToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.showMainToolStripMenuItem.Text = "Show Main";
+            this.showMainToolStripMenuItem.Click += new System.EventHandler(this.showMainToolStripMenuItem_Click);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Enabled = false;
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Enabled = false;
+            this.btn_Start.Location = new System.Drawing.Point(50, 60);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(100, 60);
+            this.btn_Start.TabIndex = 1;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(240, 60);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(100, 60);
+            this.btn_Stop.TabIndex = 2;
+            this.btn_Stop.Text = "Stop";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Watch interval(min):";
+            // 
+            // nud_interval
+            // 
+            this.nud_interval.Location = new System.Drawing.Point(217, 197);
+            this.nud_interval.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.nud_interval.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud_interval.Name = "nud_interval";
+            this.nud_interval.Size = new System.Drawing.Size(51, 21);
+            this.nud_interval.TabIndex = 4;
+            this.nud_interval.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud_interval.ValueChanged += new System.EventHandler(this.nud_interval_ValueChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.nud_interval);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Andon WatchDog";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nud_interval)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showMainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nud_interval;
+    }
+}
+
---
+++ AndonWatchDog/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Timers;
+using System.Windows.Automation;
+using System.Windows.Forms;
+
+namespace AndonWatchDog
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            try
+            {
+                if (File.Exists("interval.txt"))
+                {
+                    int.TryParse(System.IO.File.ReadAllLines("interval.txt")[0], out int tmpInterval);
+
+                    interval = 1000 * 60 * tmpInterval;
+                    nud_interval.Value = tmpInterval;
+                }
+                else
+                {
+                    File.Create("interval.txt").Close();
+                    File.WriteAllText("interval.txt", "10");
+
+                }
+
+                if (File.Exists("webTitle.txt"))
+                {
+                    webTitle = System.IO.File.ReadAllLines("webTitle.txt")[0];
+
+                }
+                else
+                {
+                    File.Create("webTitle.txt").Close();
+                    File.WriteAllText("webTitle.txt", "百度一下，你就知道");
+
+                }
+
+            }
+            catch
+            {
+
+
+            }
+        }
+
+
+
+        int interval = 1000 * 60 * 1;
+        System.Threading.Timer timer;
+        string webTitle = "百度一下，你就知道";
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("确定要关闭程序吗？\r\n[OK]关闭程序\r\n[Cancel]取消关闭\r\n提示：可最小化窗口并显示在通知栏！", "Andon WatchDog", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                //this.notifyIcon1.Visible = true;
+
+                //e.Cancel = true;
+            }
+            else
+            {
+                e.Cancel = true;
+                //if (notifyIcon1.Visible)
+                //{
+                //    this.WindowState = FormWindowState.Minimized;
+                //    this.notifyIcon1.Visible = true;
+
+                //}
+
+            }
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //this.notifyIcon1.Visible = false;
+            this.Close();
+            this.Dispose();
+
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var result = timer.Change(int.MaxValue, int.MaxValue);
+            Debug.Print("stopToolStripMenuItem_Click:" + result.ToString());
+
+            btn_Start.Enabled = true;
+            btn_Stop.Enabled = false;
+            this.startToolStripMenuItem.Enabled = true;
+            this.stopToolStripMenuItem.Enabled = false;
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var result = timer.Change(interval, interval);
+            Debug.Print("startToolStripMenuItem_Click:" + result.ToString());
+
+            btn_Start.Enabled = false;
+            btn_Stop.Enabled = true;
+            this.startToolStripMenuItem.Enabled = false;
+            this.stopToolStripMenuItem.Enabled = true;
+        }
+
+        private void showMainToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //this.notifyIcon1.Visible = false;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+
+                this.ShowInTaskbar = false;
+                //this.notifyIcon1.Visible = true;
+
+            }
+
+            if (this.WindowState == FormWindowState.Normal)
+            {
+
+                this.ShowInTaskbar = true;
+                //this.notifyIcon1.Visible = false;
+
+            }
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+
+            //if (this.WindowState == FormWindowState.Normal)
+            //{
+            //    this.WindowState = FormWindowState.Minimized;
+            //    this.ShowInTaskbar = false;
+
+            //}
+            //if (this.WindowState == FormWindowState.Minimized)
+            //{
+            //    this.WindowState = FormWindowState.Normal;
+            //    this.ShowInTaskbar = true;
+
+            //}
+            //this.notifyIcon1.Visible = false;
+
+        }
+
+        private void nud_interval_ValueChanged(object sender, EventArgs e)
+        {
+            this.interval = (int)nud_interval.Value * 1000 * 60;
+
+            File.WriteAllText("interval.txt", ((int)nud_interval.Value).ToString());
+
+
+
+            if (btn_Stop.Enabled == true && btn_Start.Enabled == false)
+            {
+                var result = timer?.Change(interval, interval);
+                Debug.Print("nud_interval_ValueChanged:" + result.ToString());
+
+            }
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+            timer = new System.Threading.Timer(timer_Callback, null, interval, interval);
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void timer_Callback(object state)
+        {
+
+            SetWindowsTop();
+            Debug.Print(DateTime.Now.ToLongTimeString());
+            //timer.Change
+
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.notifyIcon1.Visible = false;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        private WindowPattern GetWindowPattern(AutomationElement targetControl)
+        {
+            WindowPattern windowPattern = null;
+
+            try
+            {
+                windowPattern = targetControl.GetCurrentPattern(WindowPattern.Pattern) as WindowPattern;
+            }
+            catch (InvalidOperationException)
+            {
+                // object doesn't support the WindowPattern control pattern
+                return null;
+            }
+            // Make sure the element is usable.
+            if (false == windowPattern.WaitForInputIdle(10000))
+            {
+                windowPattern.SetWindowVisualState(WindowVisualState.Maximized);
+
+                // Object not responding in a timely manner
+                return null;
+            }
+            return windowPattern;
+        }
+
+
+
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+
+        private void SetWindowsTop()
+        {
+            Process[] procsEdge = Process.GetProcessesByName("msedge");
+            Debug.WriteLine("procsEdge:" + procsEdge.Length);
+
+            foreach (Process process in procsEdge)
+            {
+                // the chrome process must have a window
+                if (process.MainWindowHandle == IntPtr.Zero)
+                {
+                    ShowWindow(process.MainWindowHandle, 1);
+
+                    continue;
+                }
+
+                AutomationElementCollection roots = AutomationElement.RootElement.FindAll(TreeScope.Element | TreeScope.Children,
+                    new AndCondition(new PropertyCondition(AutomationElement.ProcessIdProperty, process.Id),
+                    new PropertyCondition(AutomationElement.ClassNameProperty, "Chrome_WidgetWin_1")));
+                Debug.WriteLine("roots:" + roots.Count);
+
+                foreach (AutomationElement rootElement in roots)
+                {
+                    Debug.WriteLine("rootElement:" + rootElement.ToString());
+
+                    AutomationElement address = rootElement.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.NameProperty, webTitle));
+                    if (address == null)
+                    {
+
+                        address = rootElement.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.NameProperty, webTitle));
+                        if (address == null)
+                        {
+                            continue;
+                        }
+                    }
+                    // 最大化窗口
+                    ShowWindow(process.MainWindowHandle, 3);
+                    address.SetFocus();
+
+                    // var v = address.GetCurrentPattern(ValuePattern.Pattern);
+                    //ValuePattern v = (ValuePattern)address.GetCurrentPattern(ValuePattern.Pattern);
+                    //Debug.WriteLine("type:" + v.GetType());
+                    //if (v.Current.Value != null && v.Current.Value != "")
+                    //{
+
+                    //    Debug.WriteLine("URL:" + v.Current.Value);
+                    //}
+
+                }
+
+            }
+
+        }
+
+
+
+
+
+
+
+    }
+}
